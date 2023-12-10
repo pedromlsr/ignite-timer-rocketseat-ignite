c3e664fbb619b8c7b705ed7735bb77662e543b65
pedromlsr <pedrodesarodrigues@gmail.com>
2023-12-09 22:49:41 -0300

    input bonus trick

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -16,14 +16,25 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput 
             id="task" 
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto" 
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" ></option>
+            <option value="Projeto 2" ></option>
+            <option value="Projeto 3" ></option>
+            <option value="Banana" ></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput 
             type="number" 
             id="minutesAmount" 
             placeholder="00" 
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
